a8c472726de6111b5cce86e2158d91a1f7c186c5
yoro-daniel <schoolyoro@gmail.com>
2026-04-28 18:34:10 +0800

    add render config

--- obj/Debug/net10.0/MobileApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MobileApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobileApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c36b9a423d0fb5f3394839e26c69457c70ce694a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobileApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobileApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
